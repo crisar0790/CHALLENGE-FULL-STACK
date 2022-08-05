d6807821cb5162e776abf0c2a14cc4cda19e2489
crisar0790 <cristian.achetoni@gmail.com>
2022-08-05 10:48:33 -0300

    modelo category

---
+++ api/src/models/Category.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define(
+    "category",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }
+    }
+  );
+};
