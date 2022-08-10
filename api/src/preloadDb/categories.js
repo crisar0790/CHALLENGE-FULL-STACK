2d2286f0d4df9ce817e2d9dadaff484fb70e32f4
crisar0790 <cristian.achetoni@gmail.com>
2022-08-10 16:17:17 -0300

    creación del archivo categories para la precarga en la base de datos

---
+++ api/src/preloadDb/categories.js
@@ -0,0 +1,40 @@
+const categories = [
+    {
+        category: 'Salary'
+    },
+    {
+        category: 'Fee collection'
+    },
+    {
+        category: 'Profits'
+    },
+    {
+        category: 'Retirement pension'
+    },
+    {
+        category: 'Renting'
+    },
+    {
+        category: 'Food'
+    },
+    {
+        category: 'Service payment'
+    },
+    {
+        category: 'Payment of fees'
+    },
+    {
+        category: 'Education expenses'
+    },
+    {
+        category: 'Public transport'
+    },
+    {
+        category: 'General shopping'
+    },
+    {
+        category: 'Others'
+    }
+]
+
+module.exports = { categories };
