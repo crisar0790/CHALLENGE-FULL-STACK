50f804efd4c04491d10836a5adc4cf8a699709dc
crisar0790 <cristian.achetoni@gmail.com>
2022-08-13 01:31:40 -0300

    colocación enlaces en el footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,15 +3,16 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    height: 60px;
-    border-top: 1px solid gray;
+    height: 40px;
+    border-top: 2px solid gray;
     position: absolute;
     bottom: 0;
     width: 100%;
+    background-color: teal;
 `;
 
 const Wrapper = styled.div`
-    padding: 10px 20px;
+    padding: 10px;
     display: flex;
     aling-items: center;
     justify-content: center;
@@ -21,6 +22,7 @@ const Left = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    color: white;
 `;
 
 const Right = styled.div`
@@ -29,18 +31,27 @@ const Right = styled.div`
     justify-content: space-evenly;
 `;
 
+const Link = styled.a`
+    display: flex;
+    align-items: center;
+`;
+
 const Footer = () => {
-  return (
-    <Container>
-        <Wrapper>
-            <Left>Cristian Ariel Achetoni</Left>
-            <Right>
-                <BsLinkedin style={{ fontSize: "1em", marginLeft: "25px", cursor: "pointer" }} />
-                <BsGithub style={{ fontSize: "1em", marginLeft: "25px", cursor: "pointer" }} />
-            </Right>
-        </Wrapper>
-    </Container>
-  )
+    return (
+        <Container>
+            <Wrapper>
+                <Left>Cristian Ariel Achetoni</Left>
+                <Right>
+                    <Link target="_blank" href='https://www.linkedin.com/in/cristian-ariel-achetoni-developer/'>
+                        <BsLinkedin style={{ fontSize: "1em", marginLeft: "25px", color: "white", cursor: "pointer" }} />
+                    </Link>
+                    <Link target="_blank" href='https://github.com/crisar0790'>
+                        <BsGithub style={{ fontSize: "1em", marginLeft: "25px", color: "white", cursor: "pointer" }} />
+                    </Link>
+                </Right>
+            </Wrapper>
+        </Container>
+    )
 }
 
 export default Footer;
