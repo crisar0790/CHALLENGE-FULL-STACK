2021f9f6ecdfeb9cda1385268b88030abc892fb4
crisar0790 <cristian.achetoni@gmail.com>
2022-08-17 14:31:00 -0300

    modificación en requestMethods

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -3,7 +3,11 @@ import axios from 'axios';
 const BASE_URL = REACT_APP_BASE_URL;
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
+export const publicRequest = axios.create({
+    baseURL: BASE_URL,
+});
+
 export const userRequest = axios.create({
     baseURL: BASE_URL,
     headers: {token:`Bearer ${TOKEN}`},
-})
+});
