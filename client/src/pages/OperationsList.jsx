09362e1855b9fd58e64a383bc750ab7cc3fea648
crisar0790 <cristian.achetoni@gmail.com>
2022-08-17 12:34:28 -0300

    modificaciones en OperationsList

--- client/src/pages/OperationsList.jsx
+++ client/src/pages/OperationsList.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import styled from "styled-components";
 import Filters from '../components/Filters';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Operations from '../components/Operations';
 import Balance from '../components/Balance';
 
+const Container = styled.div`
+    width: 100%;
+    padding-top: 6em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
 const OperationsList = () => {
   return (
     <div>
-        <Navbar />
+      <Navbar />
+      <Container>
         <Balance />
         <Filters />
         <Operations />
-        <Footer />
+      </Container>
+      <Footer />
     </div>
   )
 }
