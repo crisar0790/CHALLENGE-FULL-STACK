3903332a1c3ccc339f92594c0bf93ffbce867e20
crisar0790 <cristian.achetoni@gmail.com>
2022-08-18 13:19:15 -0300

    modificación del archivo auth en services

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,35 +1,36 @@
 import axios from "axios";
 const API_URL = process.env.REACT_APP_BASE_URL;
 
-const register = (firstName, lastName, email, password) => {
-  return axios.post(API_URL + "/auth/register", {
-    firstName,
-    lastName,
-    email,
-    password,
-  });
-};
-
-const login = (email, password) => {
-  return axios
-    .post(API_URL + "/auth/login", {
-      email,
-      password,
-    })
-    .then((response) => {
-      if (response.data.accessToken) {
-        localStorage.setItem("user", JSON.stringify(response.data));
-      }
-      return response.data;
+const register = async (firstName, lastName, email, password) => {
+    return await axios.post(API_URL + "/auth/register", {
+        firstName,
+        lastName,
+        email,
+        password,
     });
 };
 
+const login = async (email, password) => {
+    console.log(`${API_URL}/auth/login`)
+    return await axios
+        .post(`${API_URL}/auth/login`, {
+            email,
+            password,
+        })
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+            return response.data;
+        });
+};
+
 const logout = () => {
-  localStorage.removeItem("user");
+    localStorage.removeItem("user");
 };
 
 export default {
-  register,
-  login,
-  logout,
+    register,
+    login,
+    logout,
 };
