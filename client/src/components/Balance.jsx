b403c6627a68bb58e5de8092ccb70cfffe776338
crisar0790 <cristian.achetoni@gmail.com>
2022-08-19 10:02:23 -0300

    modificaciones en balance

--- client/src/components/Balance.jsx
+++ client/src/components/Balance.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
-import { getBalance } from '../actions/operations';
 
 const Container = styled.div`
     width: 100%;
