dec3f085f616c75ba7a1658cfa282cb431a39318
crisar0790 <cristian.achetoni@gmail.com>
2022-08-22 01:29:58 -0300

    modificaciones en operations en services

--- client/src/services/operations.js
+++ client/src/services/operations.js
@@ -27,18 +27,36 @@ const getLastOperations = async () => {
     }
 };
 
-const getOperations = async (type, category, order = 'des') => {
+async function getOperations (type, category, order) {
     try {
         const userId = JSON.parse(localStorage.getItem("user")).dataValues.id
-        if (userId) {
-            return await axios(`${API_URL}/operations/?userId=${userId}`, { headers: authHeader() }, { type, category, order })
+        if (userId && type === '' && category === '' && order) {
+            return await axios(`${API_URL}/operations/?userId=${userId}&order=${order}`, { headers: authHeader() })
                 .then((response) => response.data);
-        }
+        } else if (userId && type && category === '' && order === '') {
+            return await axios(`${API_URL}/operations/?userId=${userId}&type=${type}`, { headers: authHeader() })
+                .then((response) => response.data);
+        } else if (userId && type === '' && category && order === '') {
+            return await axios(`${API_URL}/operations/?userId=${userId}&category=${category}`, { headers: authHeader() })
+                .then((response) => response.data);
+        } else if (userId && type && category && order === '') {
+            return await axios(`${API_URL}/operations/?userId=${userId}&type=${type}&category=${category}`, { headers: authHeader() })
+                .then((response) => response.data);
+        } else if (userId && type && category === '' && order) {
+            return await axios(`${API_URL}/operations/?userId=${userId}&type=${type}&order=${order}`, { headers: authHeader() })
+                .then((response) => response.data);
+        } else if (userId && type === '' && category && order) {
+            return await axios(`${API_URL}/operations/?userId=${userId}&category=${category}&order=${order}`, { headers: authHeader() })
+                .then((response) => response.data);
+        } else if (userId && type && category && order) {
+            return await axios(`${API_URL}/operations/?userId=${userId}&type=${type}&category=${category}&order=${order}`, { headers: authHeader() })
+                .then((response) => response.data);
+        } 
     } catch (error) {
         console.log(error)
     }
 };
-
+    
 const getTypes = async () => {
     try {
         return await axios(`${API_URL}/types/`)
