a6ee7b797449aa3f86e3d6405f818d7a13447b95
crisar0790 <cristian.achetoni@gmail.com>
2022-08-24 17:14:30 -0300

    modificación para redireccionar a home si no se está logueado

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import OperationsList from './pages/OperationsList';
 
 function App() {
-    return (
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/operations' element={<OperationsList />} />
-      </Routes>
-    );
-  }
+  const user = JSON.parse(localStorage.getItem("user"));
+  return (
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route path='/operations' element={!user ? <Navigate to='/' /> : <OperationsList />} />
+    </Routes>
+  );
+}
 
 export default App;
