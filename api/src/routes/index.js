a4d80b75c28ebb990e376677616878dc3077cc81
crisar0790 <cristian.achetoni@gmail.com>
2022-08-24 15:54:35 -0300

    agregado de la ruta user en index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 
-const authRouter = require('./auth.js');
+const authRouter = require('./auth');
 const operationsRouter = require('./operations');
 const typesRouter = require('./types');
 const categoriesRouter = require('./categories');
+const userRouter = require('./user');
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use('/api/auth', authRouter);
 router.use('/api/operations', operationsRouter);
 router.use('/api/types', typesRouter);
 router.use('/api/categories', categoriesRouter);
+router.use('/api/user', userRouter);
 
 module.exports = router;
