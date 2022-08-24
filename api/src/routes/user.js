4579b43cc8f30045ea0b2ee2a548a77366f4500c
crisar0790 <cristian.achetoni@gmail.com>
2022-08-24 15:53:17 -0300

    creación la ruta user

---
+++ api/src/routes/user.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { verifyToken } = require('./verifyToken');
+const { User } = require('../db');
+
+router.get('/', async (req, res) => {
+
+    const { email } = req.query;
+
+    try {
+        const userFound = await User.findOne({
+            where: {email: email}
+        });
+        if (userFound.id) return res.status(200).json(userFound);
+    } catch (error) {
+        res.status(404).json(error)
+    }
+
+});
+
+module.exports = router;
